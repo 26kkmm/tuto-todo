a655bd1ea171837e725be894870f023559647930
Kaung Khant Mann Maung <itp26kkmm@gmail.com>
2024-01-16 11:21:17 +0100

    Implemented "Done Button" & "exit"

--- gui.py
+++ gui.py
@@ -3,15 +3,17 @@ from modules import functions
 
 label = sg.Text("Type a To-Do")
 input_box = sg.InputText(tooltip="Enter ToDo", key="todo")
-add_button = sg.Button("Add")
+add_button = sg.Button("add")
 list_box = sg.Listbox(
     values=functions.get_todos(), key="todos", enable_events=True, size=(45, 10)
 )
-button_edit = sg.Button("Edit")
+button_edit = sg.Button("edit")
+button_done = sg.Button("done")
+button_exit = sg.Button("exit")
 
 window = sg.Window(
     "My-To-Do App",
-    layout=[[label], [input_box, add_button], [list_box, button_edit]],
+    layout=[[label], [input_box, add_button], [list_box, button_edit, button_done]],
     font=("Helvetica", 20),
 )
 
@@ -20,7 +22,7 @@ while True:
     print(event)
     print(values)
     match event:
-        case "Add":
+        case "add":
             todos = functions.get_todos()
             new_todo = values["todo"] + "\n"
             todos.append(new_todo)
@@ -28,7 +30,7 @@ while True:
 
             window['todos'].update(values=todos)
 
-        case 'Edit':
+        case 'edit':
             todo_to_edit = values["todos"][0]
             new_todo = values["todo"]
 
@@ -42,7 +44,18 @@ while True:
         case 'todos':
             window['todo'].update(value=values['todos'][0])
 
-        case sg.WIN_CLOSED:
+        case 'done':
+            todos = functions.get_todos()
+            done = values['todo']
+            todos.remove(done)
+            functions.write_todos(todos)
+
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
+
+        case 'exit':
             break
 
+        case sg.WIN_CLOSED:
+            break
 window.close()
