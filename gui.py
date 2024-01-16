7d7efdc2c695318a42b8f792f70de314d6474ecd
Kaung Khant Mann Maung <itp26kkmm@gmail.com>
2024-01-16 17:55:27 +0100

    Changed Add Button to Image, Implement File Creating for exe, Changed Edit with popup

--- gui.py
+++ gui.py
@@ -1,23 +1,30 @@
 import PySimpleGUI as sg
 from modules import functions
 import time
+import os
+
+if not os.path.exists('todos.txt'):
+    with open('todos.txt', 'w') as file:
+        pass
 
 sg.theme('black')
-clock = sg.Text(''  , key='clock')
+clock = sg.Text('', key='clock')
 label = sg.Text("Type a ToDo")
 input_add = sg.InputText(tooltip="Enter ToDo", key="todo")
-add_button = sg.Button("add")
 list_box = sg.Listbox(
     values=functions.get_todos(), key="todos", enable_events=True, size=(40, 10)
 )
+
+button_add = sg.Button(image_source='add.png', tooltip='Add ToDo', size=10)
 button_edit = sg.Button("edit")
-button_done = sg.Button("done")
+button_done = sg.Button('done', tooltip='Done', size=8)
 button_exit = sg.Button("exit")
 
 window = sg.Window(
     "My ToDo App",
-    layout=[[clock],[label], [input_add, add_button],
-            [list_box, button_edit, button_done]],
+    layout=[[clock], [label], [input_add, button_add],
+            [list_box, button_edit, button_done],
+            [button_exit]],
     font=("Helvetica", 20),
 )
 
@@ -35,6 +42,7 @@ while True:
             functions.write_todos(todos)
 
             window['todos'].update(values=todos)
+            window['todo'].update(value='')
 
         case 'edit':
             try:
@@ -43,7 +51,7 @@ while True:
                 todo_to_edit = values["todos"][0]
 
                 new_todo = sg.popup_get_text('Edit todo:', 'Edit',
-                                               default_text=todo_to_edit)
+                                             default_text=todo_to_edit)
 
                 index = todos.index(todo_to_edit)
                 todos[index] = new_todo + "\n"
@@ -52,8 +60,6 @@ while True:
                 window['todos'].update(values=todos)
             except IndexError:
                 sg.popup("Please Select a ToDo")
-
-
 
         case 'done':
             todos = functions.get_todos()
