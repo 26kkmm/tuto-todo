bea915c6e04c75e024268186bb94033c652a43ad
Kaung Khant Mann Maung <itp26kkmm@gmail.com>
2024-01-18 14:01:06 +0100

    Implemented "Complete" feature

--- web.py
+++ web.py
@@ -3,9 +3,10 @@ from modules import functions
 
 todos = functions.get_todos()
 def add_todo():
-    todo =st.session_state['new-todo'] + '\n'
+    todo = st.session_state['new-todo'] + '\n'
     todos.append(todo)
     functions.write_todos(todos)
+
 
 
 
@@ -14,7 +15,13 @@ st.subheader("This is my app")
 st.write("Write your ToDo, Aha!")
 
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
+
 
 st.text_input(label="", placeholder='Add ToDo...', key='new-todo', on_change=add_todo)
